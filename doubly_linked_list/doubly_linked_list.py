3a405c0a68ecd9f04a11eeca316aa22901db890d
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-14 21:06:54 -0500

    Implemented delete functionality for doubly linked lists

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -108,9 +108,10 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
+    # O(1)
     def remove_from_tail(self):
 
-        # handles non-empty doubly linked lists with 2 or more nodes
+        # handles doubly linked lists with 2 or more nodes
         if self.length > 1:
             self.length -= 1 # decrement length
             oldNode = self.tail # save old tail
@@ -136,27 +137,124 @@ class DoublyLinkedList:
         # handles empty double linked lists
         else:
             return # returns None
-            
+   
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
+    # O(n)
     def move_to_front(self, node):
-        pass
+        
+        # handles doubly linked lists with 2 or more nodes
+        if self.length > 1:
+            current = self.head # start iterating at head
+
+            # iterate over linked list until node argument is found
+            while current is not node:
+                current = current.next
+
+            # set the prev node's next to node after current
+            if current.prev is not None and current.next is not None:
+                current.prev.next = current.next
+
+            # set the next node's prev to node before current
+            if current.prev is not None and current.next is not None:
+                current.next.prev = current.prev
+
+            oldHead = self.head # save old head
+            oldHead.prev = current # set old head prev to current node
+            current.prev = None # set current node's prev to None
+            current.next = oldHead # set current node's next to old head
+            self.head = current # set head to current node
+        
+        # handles all other doubly linked lists
+        else:
+            return
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
+    # O(n)
     def move_to_end(self, node):
-        pass
+        # handles doubly linked lists with 2 or more nodes
+        if self.length > 1:
+            current = self.head # start iterating at head
+
+            # iterate over linked list until node argument is found
+            while current is not node:
+                current = current.next
+
+            # handles case where the head is moved to end of 2 element doubly linked list
+            if current == self.head:
+                oldTail = self.tail # save old tail
+                oldTail.next = current # set old tail next to current node
+                self.head = current.next # set head to current's next node
+                current.next = None # set current node's next to None
+                current.prev = oldTail # set current node's prev to old tail
+                self.tail = current # set tail to current node
+            else:
+                # set the prev node's next to node after current
+                if current.prev is not None and current.next is not None:
+                    current.prev.next = current.next
+
+                # set the next node's prev to node before current
+                if current.next is not None and current.next is not None:
+                    current.next.prev = current.prev
+
+                oldTail = self.tail # save old tail
+                oldTail.next = current # set old tail next to current node
+                current.next = None # set current node's next to None
+                current.prev = oldTail # set current node's prev to old tail
+                self.tail = current # set tail to current node
+            
+        # handles empty doubly linked lists
+        else:
+            return
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
+    # O(n)
     def delete(self, node):
-        pass
+        # handles doubly linked lists with 2 or more nodes
+        if self.length > 1:
+            current = self.head # start iterating at head
+            self.length -= 1 # decrement length
+
+            # iterate over linked list until node argument is found
+            while current is not node:
+                current = current.next
+
+            # handles case when deleted node is head
+            if current is self.head:
+                current.next.prev = None # set new head's prev to None
+                self.head = current.next # set head to new head
+            
+            # handles case when deleted node is in middle of list
+            # set the prev node's next to node after current
+            elif current.prev is not None and current.next is not None:
+                current.prev.next = current.next
+
+            # set the next node's prev to node before current
+            elif current.prev is not None and current.next is not None:
+                current.next.prev = current.prev
+            
+            # handles case when deleted node is tail
+            elif current is self.tail:
+                current.prev.next = None # set new tail's next to None
+                self.tail = current.prev # set tail to new tail
+
+        elif self.length == 1:
+            self.length -= 1
+            self.head = None
+            self.tail = None
+            self.value = None
+        
+        # handles all other doubly linked lists
+        else:
+            return
 
     """
     Finds and returns the maximum value of all the nodes 
