d52296c7318684aeeb39b7c527710bade1393fee
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-15 16:45:21 -0500

    Implemented insert, search, get_max, and for_each functions for binary search trees

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,81 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # compare the input value with the value of the Node
+        # if value < Node's value
+        if value < self.value:
+            # we need to go left
+            # if we see that there is no left child,
+                if self.left is None:
+                    # then we can wrap the
+                    # value in a BSTNode and park it
+                    self.left = BSTNode(value)
+                # otherwise there is a child
+                else:            
+                    # call the left child's insert method
+                    self.left.insert(value)
+        # otherwise, value >= Node's value
+        else:
+            # we need to go this Node's right child
+            # if we see there is no right child, 
+            if self.right is None:
+                # then we can wrap the
+                # value in a BSTNode and park it
+                self.right = BSTNode(value)
+            # otherwise there is a child
+            else:
+                # call the right child's insert method
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # compare the target value with the value of the Node
+        # if target < Node's value
+        if target < self.value:
+            # we need to go left
+            # if we see that there is no left child,
+            if self.left is None:
+                # return False
+                return False
+            # otherwise there is a child
+            else:            
+                # call the left child's contains method
+                return self.left.contains(target)
+        # otherwise, target > Node's value
+        else:
+            # if target is Node's value
+            if target == self.value:
+                return True
+            # we need to go this Node's right child
+            # if we see there is no right child, 
+            if self.right is None:
+                # return False
+                return False
+            # otherwise there is a child
+            else:
+                # call the right child's contains method
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
-
+        if self.right is None:
+            return self.value
+        return self.right.get_max()
+                
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # Pre-order traversal
+        # Base case
+        if self.value is None:
+            return
+        fn(self.value)
+        if self.left is not None:
+            self.left.for_each(fn)
+        if self.right is not None:
+            self.right.for_each(fn)
+
+
 
     # Part 2 -----------------------
 
